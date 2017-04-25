07d2c7fa2cc42ee6c70635b3ec694f2a67f6a79d
msifeed <remsifeed@gmail.com>
2017-04-25 21:25:08 +0300

    Замедление инструментов из Тинкера

--- src/main/java/ru/ariadna/misca/tweaks/SlowMining.java
+++ src/main/java/ru/ariadna/misca/tweaks/SlowMining.java
@@ -1,6 +1,8 @@
 package ru.ariadna.misca.tweaks;
 
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraftforge.common.MinecraftForge;
@@ -10,6 +12,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
  * Замедляет копание иструментами. Пустую руку не затрагивает.
  */
 public class SlowMining {
+    private static Class<?> tinkerToolClass;
     private ConfigSection config;
 
     public void init() {
@@ -17,16 +20,25 @@ public class SlowMining {
 
         if (config.enabled) {
             MinecraftForge.EVENT_BUS.register(this);
+
+            try {
+                tinkerToolClass = Class.forName("tconstruct.library.tools.ToolCore");
+                Tweaks.logger.info("Tinker Construct found. Slowing it down...");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onBreakSpeed(PlayerEvent.BreakSpeed event) {
-        ItemStack held_item = event.entityPlayer.getHeldItem();
+        ItemStack heldItemStack = event.entityPlayer.getHeldItem();
+        if (heldItemStack == null) return;
 
-        if (held_item == null || !(held_item.getItem() instanceof ItemTool)) {
-            return;
-        }
+        Item helItem = heldItemStack.getItem();
+        boolean isTool = helItem instanceof ItemTool ||
+                (tinkerToolClass != null && tinkerToolClass.isInstance(helItem));
+        if (!isTool) return;
 
         if (event.newSpeed == 0) {
             event.newSpeed = event.originalSpeed;
