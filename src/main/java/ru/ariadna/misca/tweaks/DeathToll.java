e5723a1da1e2e49d74587864afc6b72a7afd3e26
msifeed <remsifeed@gmail.com>
2017-06-10 16:05:06 +0300

    Названия самих болезней при смерти

--- src/main/java/ru/ariadna/misca/tweaks/DeathToll.java
+++ src/main/java/ru/ariadna/misca/tweaks/DeathToll.java
@@ -1,14 +1,16 @@
 package ru.ariadna.misca.tweaks;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import ru.ariadna.misca.Misca;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -33,25 +35,31 @@ public class DeathToll {
         EntityPlayer player = (EntityPlayer) event.entityLiving;
         String death_source = event.source.damageType;
         ChunkCoordinates coords = player.getPlayerCoordinates();
-        String stat = getRandomStat();
-        String msg = String.format("[%s] Player '%s' died from '%s' at (%s) %d-%d-%d. Random illness of '%s'\n",
+        String illness = getRandomIllness();
+        String msg = String.format("[%s] Player '%s' died from '%s' at (%s) %d-%d-%d. Random illness '%s'\n",
                 timestamp,
                 player.getDisplayName(),
                 death_source,
                 player.getEntityWorld().getWorldInfo().getWorldName(),
                 coords.posX, coords.posY, coords.posZ,
-                stat);
+                illness);
 
         writer.append(msg);
         writer.flush();
         writer.close();
 
-        String chat_msg = "Вы получили душевную болезнь " + stat;
+        String chat_msg = "Вы заболели " + illness;
         player.addChatMessage(new ChatComponentText(chat_msg));
     }
 
-    private String getRandomStat() {
-        final String[] stats = {"силы", "рефлексов", "восприятия", "учености", "решительности", "духа"};
+    private String getRandomIllness() {
+        final String[] stats = {
+                "мышечными спазмами", "Немощью", "Ломкостью",
+                "неуклюжестью", "нарушением моторики", "Трясучкой",
+                "потерей чувств", "Неразборчивостью", "Расфокусировкой",
+                "амнезией", "Технологической слепотой", "забывчивостью",
+                "Бесхребетностью", "Раболепием", "подавлением иммунитета",
+                "Душевными метаниями", "Магочувствительностью"};
         Random rand = new Random();
         return stats[rand.nextInt(stats.length)];
     }
