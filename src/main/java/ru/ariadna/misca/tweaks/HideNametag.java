b1175f7427e84160c78df8f2b04acf027cdddf5e
msifeed <remsifeed@gmail.com>
2017-07-14 12:14:39 +0300

    Отображение имен игроков в пределах трех блоков

--- src/main/java/ru/ariadna/misca/tweaks/HideNametag.java
+++ src/main/java/ru/ariadna/misca/tweaks/HideNametag.java
@@ -3,18 +3,20 @@ package ru.ariadna.misca.tweaks;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderLivingEvent;
 
 /**
- * Отменяет рендеринг плашки с именем над игроком
+ * Отменяет рендеринг плашки с именем над игроком дальше 3 блоков
  */
 public class HideNametag {
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void onRenderLivingSpecial(RenderLivingEvent.Specials.Pre event) {
-        if (event.entity instanceof EntityPlayer && event.isCancelable()) {
+        EntityPlayer self = Minecraft.getMinecraft().thePlayer;
+        if (event.entity instanceof EntityPlayer && event.isCancelable() && self.getDistanceToEntity(event.entity) > 3) {
             event.setCanceled(true);
         }
     }
