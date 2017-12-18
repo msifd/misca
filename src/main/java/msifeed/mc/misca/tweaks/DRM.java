efc164394602071327d43cb17ffd82e272b65888
msifeed <remsifeed@gmail.com>
2017-12-19 00:50:51 +0300

    Фикс DRM для работы с ip

--- src/main/java/msifeed/mc/misca/tweaks/DRM.java
+++ src/main/java/msifeed/mc/misca/tweaks/DRM.java
@@ -1,21 +1,26 @@
 package msifeed.mc.misca.tweaks;
 
+import com.google.common.net.InetAddresses;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 
 import java.awt.*;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.URI;
 
 public class DRM {
+    private static final InetAddress LOCALHOST = InetAddresses.forString("127.0.0.1");
+    private static final InetAddress ARIADNA = InetAddresses.forString("46.105.37.9");
+
     @SubscribeEvent
     public void onClientConnectedToServerEvent(ClientConnectedToServerEvent event) {
-        SocketAddress address = event.manager.getSocketAddress();
-        if (!(address instanceof InetSocketAddress)) return;
+        final SocketAddress socket = event.manager.getSocketAddress();
+        if (!(socket instanceof InetSocketAddress)) return;
 
-        String host = ((InetSocketAddress) address).getHostString();
-        if (host.equals("localhost") || host.equals("ariadna-rp.ru")) return;
+        final InetAddress address = ((InetSocketAddress) socket).getAddress();
+        if (address.equals(LOCALHOST) || address.equals(ARIADNA)) return;
 
         if (Desktop.isDesktopSupported()) {
             try {
