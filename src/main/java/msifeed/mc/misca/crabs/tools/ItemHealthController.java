3551a8c1b37376cfa207bca832aad3ab079b1231
Ikamari <SilverFire098@gmail.com>
2018-01-26 22:48:17 +0200

    Фиск с пропусками строк в описании, действия на себе

--- src/main/java/msifeed/mc/misca/crabs/tools/ItemHealthController.java
+++ src/main/java/msifeed/mc/misca/crabs/tools/ItemHealthController.java
@@ -1,10 +1,10 @@
 package msifeed.mc.misca.crabs.tools;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import msifeed.mc.misca.things.MiscaThings;
-import msifeed.mc.misca.utils.MiscaUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,7 +12,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import org.apache.commons.lang3.StringEscapeUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ItemHealthController extends Item {
@@ -21,6 +23,18 @@ public class ItemHealthController extends Item {
         setTextureName("nether_star");
         setCreativeTab(MiscaThings.itemsTab);
         setMaxStackSize(1);
+    }
+
+    public void addHealth(EntityLivingBase entity, Float amount) {
+        final Float currentEntityHealth = entity.getHealth();
+        if (currentEntityHealth <= entity.getMaxHealth())
+            entity.setHealth(entity.getHealth() + amount);
+    }
+
+    public void removeHealth(EntityLivingBase entity, Float amount) {
+        final Float currentEntityHealth = entity.getHealth();
+        if (currentEntityHealth > 1.0F)
+            entity.setHealth(entity.getHealth() - amount);
     }
 
     @Override
@@ -35,31 +49,55 @@ public class ItemHealthController extends Item {
 
     @Override
     public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean advanced) {
-        lines.add(MiscaUtils.l10n("item.health_controller.desc"));
+        String desc = LanguageRegistry.instance().getStringLocalization("item.health_controller.desc");
+        desc = StringEscapeUtils.unescapeJava(desc);
+        lines.addAll(Arrays.asList(desc.split("\n")));
+    }
+
+    @Override
+    public boolean onEntitySwing(EntityLivingBase player, ItemStack stack) {
+        // Shift+ЛКМ - добавить 1 хп себе
+
+        if (player.isSneaking()) {
+            this.addHealth(player, 1.0F);
+        }
+
+        return true;
     }
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity target) {
-        // ЛКМ по существу: добавить 1 хп
-        if (player.worldObj.isRemote || !(target instanceof EntityLivingBase)) return true;
+        // ЛКМ по существу - добавить 1 хп
 
-        final EntityLivingBase entity = (EntityLivingBase) target;
-        final Float currentEntityHealth = entity.getHealth();
-        if (currentEntityHealth <= entity.getMaxHealth())
-            entity.setHealth(entity.getHealth() + 1.0F);
+        if (!player.isSneaking()) {
+            if (player.worldObj.isRemote || !(target instanceof EntityLivingBase)) return true;
+            final EntityLivingBase entity = (EntityLivingBase) target;
+            this.addHealth(entity, 1.0F);
+        }
 
         return true;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        // Shift+ПКМ - убрать 1 хп себе
+
+        if (player.isSneaking()) {
+            this.removeHealth(player, 1.0F);
+        }
+
+        return stack;
     }
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void onEntityInteract(EntityInteractEvent event) {
         // ПКМ по существу: убрать 1 хп
-        if (!(event.target instanceof EntityLivingBase)) return;
 
-        final EntityLivingBase entity = (EntityLivingBase) event.target;
-        final Float currentEntityHealth = entity.getHealth();
-        if (currentEntityHealth > 1.0F)
-            entity.setHealth(entity.getHealth() - 1.0F);
+        if (event.entityPlayer.isSneaking()) {
+            if (!(event.target instanceof EntityLivingBase)) return;
+            final EntityLivingBase entity = (EntityLivingBase) event.target;
+            this.removeHealth(entity, 1.0F);
+        }
     }
 }
