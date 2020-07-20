72943ab3398a7c36d3916bcfb4235bfcea1431b5
msifeed <remsifeed@gmail.com>
2020-07-21 00:08:07 +0300

    Init 1.12.2

---
+++ src/main/java/msifeed/misca/Misca.java
@@ -0,0 +1,28 @@
+package msifeed.misca;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Misca.MODID, name = Misca.NAME, version = Misca.VERSION)
+public class Misca {
+    public static final String MODID = "misca";
+    public static final String NAME = "Misca";
+    public static final String VERSION = "2.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
