a700e3d80a1acd4d8cd7f6de1f8f7a9a3c509246
msifeed <remsifeed@gmail.com>
2021-03-16 09:37:47 +0300

    Optimize unpacked quads and mute achievement errors

---
+++ src/main/java/msifeed/misca/mixins/AdvancementManagerMixin.java
@@ -0,0 +1,25 @@
+package msifeed.misca.mixins;
+
+import net.minecraft.advancements.AdvancementManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(AdvancementManager.class)
+public class AdvancementManagerMixin {
+    @Redirect(method = "loadCustomAdvancements", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+    private void onCustomLogError(Logger logger, String message, Throwable t) {
+        logSomeErrors(logger, message, t);
+    }
+
+    @Redirect(method = "loadBuiltInAdvancements", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+    private void onBuildInLogError(Logger logger, String message, Throwable t) {
+        logSomeErrors(logger, message, t);
+    }
+
+    private void logSomeErrors(Logger logger, String message, Throwable t) {
+        if (!message.startsWith("Parsing"))
+            logger.error(message, t);
+    }
+}
