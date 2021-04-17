20657493b4a3bde6afa5017cff61366c40e34508
msifeed <remsifeed@gmail.com>
2021-04-17 23:36:37 +0300

    Fix chat hud mixin again

--- src/main/java/msifeed/misca/mixins/client/GuiIngameMixin.java
+++ src/main/java/msifeed/misca/mixins/client/GuiIngameMixin.java
@@ -4,16 +4,18 @@ import msifeed.misca.chatex.client.gui.ChatexHud;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiIngame.class)
 public abstract class GuiIngameMixin {
-    protected GuiNewChat persistantChatGUI;
+    @Accessor
+    public abstract void setPersistantChatGUI(GuiNewChat chat);
 
     @Inject(at = @At("RETURN"), method = "<init>")
     public void constructor(CallbackInfo ci) {
-        persistantChatGUI = new ChatexHud();
+        setPersistantChatGUI(new ChatexHud());
     }
 }
