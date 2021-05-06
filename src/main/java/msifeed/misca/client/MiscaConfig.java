10616cf121112d9f9d0f43ded4967beb2822eb58
msifeed <remsifeed@gmail.com>
2021-05-06 22:34:45 +0300

    Fix config categories

--- src/main/java/msifeed/misca/client/MiscaConfig.java
+++ src/main/java/msifeed/misca/client/MiscaConfig.java
@@ -4,7 +4,7 @@ import msifeed.mellow.utils.Point;
 import msifeed.misca.Misca;
 import net.minecraftforge.common.config.Config;
 
-@Config(modid = Misca.MODID)
+@Config(modid = Misca.MODID, name = "client")
 public class MiscaConfig {
     public static String windowTitle = "Ariadna";
     public static boolean combatDebug = false;
